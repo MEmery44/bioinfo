e835310ac96c126217e981adace319ef91098c47
DeadHand49 <matthew.emery44@gmail.com>
2015-09-20 12:07:34 -0700

    Finished eulerian path

--- bioinfo2wk2/euler_tour.py
+++ bioinfo2wk2/euler_tour.py
@@ -1,8 +1,8 @@
 __author__ = 'memery'
 
-from collections import defaultdict
 import networkx as nx
-from random import choice
+
+
 
 def eulerian_cycle(graph):
     return [i for i in nx.eulerian_circuit(graph)]
@@ -20,40 +20,19 @@ def make_graph(*edges):
     return graph
 
 
-def make_graph_default(*edges):
-    graph = defaultdict(list)
-    for edge in edges:
-        start, end = edge.split(' -> ')
-        if ',' in end:
-            for node in end.split(','):
-                graph[start].append((node.strip(), False))
-        else:
-            graph[start].append((end.strip(), False))
-    return graph
 
-
-def default_eulerian_cycle(graph):
-    """
-
-    :param graph:
-    :type graph: defaultdict
-    """
-    to_be_traveled = graph.copy()
-    path = []
-    new_node = choice(list(graph.keys()))
-    path.append(new_node[0])
-    while len(to_be_traveled) != 0:
-        node = to_be_traveled[new_node[0]]
-        if
-        if type(node) == list:
-            new_node = choice(node)
-            new_node = new_node[0], True
-        else:
-            if new
-            new_node = node
-            new_node = new_node[0], True
-        path.append(new_node[0])
-    return path
+def add_eulerian_bridge(G):
+    [start_node] = [x for x in G.in_degree() if G.in_degree(x) - G.out_degree(x) == 1]
+    [end_node] = [x for x in G.in_degree() if G.in_degree(x) - G.out_degree(x) == -1]
+    G.add_edge(start_node, end_node)
+    cycle = eulerian_cycle(G)
+    for i in range(len(cycle)):
+        if cycle[i] == (start_node, end_node):
+            prefix, suffix = cycle[:i], cycle[i+1:]
+            del cycle[i]
+            cycle = suffix + prefix
+            break
+    return cycle
 
 
 def print_eulerian(*euler):
@@ -62,8 +41,14 @@ def print_eulerian(*euler):
         path = '{}->{}'.format(path, euler[i][1])
     return path
 
+def make_de_bruijin_graph(k, *kmwers):
+    grsph = nx.
+
+
 
 if __name__ == '__main__':
-    graph = make_graph_default(
-        *('0 -> 3', '1 -> 0', '2 -> 1,6', '3 -> 2', '4 -> 2', '5 -> 4', '6 -> 5,8', '7 -> 9', '8 -> 7', '9 -> 6'))
-    print(default_eulerian_cycle(graph))
+    with open('dataset_203_5.txt') as data:
+        graph = make_graph(*(x.strip() for x in data.readlines()))
+    cycle = add_eulerian_bridge(graph)
+    with open('output.txt', 'w+') as out:
+        out.write(print_eulerian(*cycle))
