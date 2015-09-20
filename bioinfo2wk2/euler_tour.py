4e358c9a00ce938df91de7a2e596510fa08dab9b
DeadHand49 <matthew.emery44@gmail.com>
2015-09-20 13:10:54 -0700

    Up double read string

--- bioinfo2wk2/euler_tour.py
+++ bioinfo2wk2/euler_tour.py
@@ -1,7 +1,7 @@
 __author__ = 'memery'
 
 import networkx as nx
-
+from itertools import product
 
 
 def eulerian_cycle(graph):
@@ -20,7 +20,6 @@ def make_graph(*edges):
     return graph
 
 
-
 def add_eulerian_bridge(G):
     [start_node] = [x for x in G.in_degree() if G.in_degree(x) - G.out_degree(x) == 1]
     [end_node] = [x for x in G.in_degree() if G.in_degree(x) - G.out_degree(x) == -1]
@@ -28,7 +27,7 @@ def add_eulerian_bridge(G):
     cycle = eulerian_cycle(G)
     for i in range(len(cycle)):
         if cycle[i] == (start_node, end_node):
-            prefix, suffix = cycle[:i], cycle[i+1:]
+            prefix, suffix = cycle[:i], cycle[i + 1:]
             del cycle[i]
             cycle = suffix + prefix
             break
@@ -41,14 +40,33 @@ def print_eulerian(*euler):
         path = '{}->{}'.format(path, euler[i][1])
     return path
 
-def make_de_bruijin_graph(k, *kmwers):
-    grsph = nx.
+
+def make_de_bruijin_graph(*kmers): #used to have a k. see if this matters
+    graph = nx.DiGraph()
+    for kmer in kmers:
+        graph.add_edge(kmer[:-1], kmer[1:])
+    return graph
+
+def reconstruct_string(graph):
+    path = add_eulerian_bridge(graph)
+    reconstructed = path[0][0]
+    for i in path:
+        reconstructed = '{}{}'.format(reconstructed, i[1][-1])
+    return reconstructed
+
+def reconstruct_uni_string(k, graph):
+    cycle = eulerian_cycle(graph)
+    reconstructed = cycle[0][0]
+    for i in cycle[:-k+1]:
+        reconstructed = '{}{}'.format(reconstructed, i[1][-1])
+    return reconstructed
 
 
+def generate_binary_strings(size):
+    return [''.join(x) for x in product('01', repeat=int(size))]
 
 if __name__ == '__main__':
-    with open('dataset_203_5.txt') as data:
-        graph = make_graph(*(x.strip() for x in data.readlines()))
-    cycle = add_eulerian_bridge(graph)
+    graph = make_de_bruijin_graph(*generate_binary_strings(8))
     with open('output.txt', 'w+') as out:
-        out.write(print_eulerian(*cycle))
+        out.write(reconstruct_uni_string(8, graph))
+    # print(reconstruct_uni_string(4, graph))
