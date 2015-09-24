037e913bfac584ce22805c8d36501eccaaf82edc
DeadHand49 <matthew.emery44@gmail.com>
2015-09-24 15:11:25 -0700

    Profile most probable kmer finished

--- bioinfo1wk3/median_string.py
+++ bioinfo1wk3/median_string.py
@@ -2,6 +2,7 @@ __author__ = 'memery'
 
 from itertools import product
 
+
 def distance_between_pattern_and_string(pattern, dna):
     distance = 0
     for text in dna:
@@ -12,6 +13,7 @@ def distance_between_pattern_and_string(pattern, dna):
         distance += hamming_distance
     return distance
 
+
 def get_hamming_distance(first_string, second_string):
     assert len(first_string) == len(second_string)
     distance = 0
@@ -19,6 +21,7 @@ def get_hamming_distance(first_string, second_string):
         if first_string[i] != second_string[i]:
             distance += 1
     return distance
+
 
 def median_string(dna, k):
     distance = float('infinity')
@@ -28,10 +31,41 @@ def median_string(dna, k):
             median = pattern
     return median
 
+
 def generate_dna_strings(size):
     return [''.join(x) for x in product('ACGT', repeat=int(size))]
 
+
+def profile_most_probable_kmer(text, k, matrix):
+    profile_score = 0
+    profile_text = ''
+    for i in range(len(text) - k):
+        if get_profile_score(text[i:i + k], matrix) > profile_score:
+            profile_score = get_profile_score(text[i:i + k], matrix)
+            profile_text = text[i:i + k]
+    return profile_text
+
+
+def get_profile_score(text, matrix):
+    dna_map = {'A': 0,
+               'C': 1,
+               'G': 2,
+               'T': 3}
+    total = 1
+    for i in range(len(text)):
+        total *= matrix[dna_map[text[i]]][i]
+    return total
+
+
+def make_matrix(*strings):
+    matrix = []
+    for line in strings:
+        matrix.append([float(x) for x in line.split(' ')])
+    return matrix
+
+
 if __name__ == '__main__':
-    with open('dataset_158_9.txt') as data:
+    with open('dataset_159_3.txt') as data:
         lines = [x.strip() for x in data.readlines()]
-        print(median_string(lines[1:], int(lines[0])))
+        matrix = make_matrix(*(lines[2:]))
+        print(profile_most_probable_kmer(lines[0], int(lines[1]), matrix))
